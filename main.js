e2536dafb23f9975d28d0e0f5c8591553f14430b
ji <jiyongprk@gmail.com>
2022-02-20 16:14:55 -0500

    right button working

--- main.js
+++ main.js
@@ -4,49 +4,77 @@ const rightBtn = document.querySelector('.right-btn');
 const randomBtn = document.querySelector('.random-btn');
 
 
-const image = document.querySelector('.img-container');
+const image = document.querySelector('img');
 const fullName = document.querySelector('.name');
 const job = document.querySelector('.job');
 const text = document.querySelector('.text');
 
 const friendList = [{
     id: 1,
-    name: "John 1",
-    job: "soft",
-    img: "https://pbs.twimg.com/profile_images/1465489487757201410/38MVGVDX_400x400.https://pbs.twimg.com/profile_images/1466755254645633026/90lz2kwx_400x400.jpg",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
+    name: "John Doe",
+    job: "Software Developer",
+    img: "https://pbs.twimg.com/profile_images/1280134466762166272/3OQXdBwC_400x400.jpg",
+    text: "11111Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
 },
 {
     id: 2,
-    name: "John 2",
-    job: "software",
+    name: "Patrick Star",
+    job: "Software Engineer",
     img: "https://pbs.twimg.com/profile_images/1480879644618510336/2iXc8iDk_400x400.jpg",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
+    text: "2222Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
 },
 {
     id: 3,
-    name: "John 3",
-    job: "software dev",
+    name: "Spongebob",
+    job: "Senior Developer",
     img: "https://pbs.twimg.com/profile_images/1459203645782831117/GURd33BT_400x400.jpg",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
+    text: "33333Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
 },
 {
     id: 4,
-    name: "John 4",
-    job: "software devloper",
+    name: "Sandy Squirrel",
+    job: "Senior Engineer",
     img: "https://pbs.twimg.com/profile_images/1435888634612301827/aUqoMcQ-_400x400.jpg",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
+    text: "44444Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem numquam assumenda, quia placeat modi saepe ex repellendus ipsum ea odio ad fuga voluptate molestias necessitatibus explicabo! Consequuntur amet rerum itaque, debitis inventore sed laboriosam similique accusamus aliquam. Enim, at unde."
 }];
 
-leftBtn.addEventListener('click', ()=>{
-    let friendOne = friendList.map((friend)=>{
-        if (friend.name === "John 1"){
-            return friend.name;
-        }
-    })
-    console.log(friendOne);
-    fullName.textContent = friendOne.join('');
+let friends = 0; // 0-3 since there are 4 objects
+
+
+rightBtn.addEventListener('click', ()=>{
+    friends++;
+    console.log(friends);
+    if (friends >= 3){
+        friends = 0;
+    } 
+    showPerson(friends);
 })
+
+leftBtn.addEventListener('click', ()=>{
+
+})
+
+function showPerson(person){
+    let whichFriend = friendList[person];
+    console.log(whichFriend);
+    image.src = whichFriend.img;
+    fullName.textContent = whichFriend.name;
+    job.textContent = whichFriend.job;
+    text.textContent = whichFriend.text;
+}
+
+
+
+// leftBtn.addEventListener('click', ()=>{
+//     let friendOne = friendList.map((friend)=>{
+//         if (friend.name === "John 1"){
+//             return friend.name;
+//         }
+//     })
+//     console.log(friendOne);
+//     fullName.textContent = friendOne.join('');
+//     image.src = friendList.job;
+// })
 
 
 
